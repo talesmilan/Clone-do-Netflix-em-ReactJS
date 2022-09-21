b9687984b2c819b8c2295ad07ce7a9138f838be8
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-21 19:57:31 -0300

    Criando a lista

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,1 +1,49 @@
 const API_KEY = '969f8267c98ccb5dcf8b40f2e3f200b3'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: []
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para Você',
+                items: []
+            },
+            {
+                slug: 'toprated',
+                title: 'Em Alta',
+                items: []
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: []
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: []
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: []
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: []
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: []
+            }
+        ]
+    } 
+}
