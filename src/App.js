496a2bb833a2411ae572027864ca6efc3885b3bd
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-22 14:13:43 -0300

    Fazendo a lista de filmes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
+import './App.css'
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([])
 
   useEffect(() => {
     const loadAll = async () => {
       //Pegando a lista dos filmes
       let list = await Tmdb.getHomeList()
-      console.log(list)
+      setMovieList(list)
     }
 
     loadAll()
@@ -17,8 +21,13 @@ export default () => {
 
 
   return (
-    <div>
-      Olá mundo!
+    <div className="page">
+      <section className="lists">
+      
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   )
 } 
