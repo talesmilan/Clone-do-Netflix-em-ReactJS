99dc87606b74705745d5bbf0461f49e0e51ee551
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-23 11:57:44 -0300

    Fazendo o fundo

---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './FeaturedMovie.css'
+
+export default ({item}) => {
+    return (
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured-info">
+                        <div className="featured--points">Nota: {item.vote_average}</div>
+                        <div className="featured--year">2099</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
