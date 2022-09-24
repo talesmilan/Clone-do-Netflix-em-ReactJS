b0f4163969d9199ad7f72b95ff79bf7c7b4af2a8
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-24 15:52:39 -0300

    Estilizando os botões

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,6 +2,14 @@ import React from 'react'
 import './FeaturedMovie.css'
 
 export default ({item}) => {
+    console.log(item)
+
+    let firstDate = new Date(item.first_air_date)
+    let genres = []
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -11,9 +19,16 @@ export default ({item}) => {
             <div className="featured--vertical">
                 <div className="featured--horizontal">
                     <div className="featured--name">{item.original_name}</div>
-                    <div className="featured-info">
+                    <div className="featured--info">
                         <div className="featured--points">Nota: {item.vote_average}</div>
-                        <div className="featured--year">2099</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
+                        <div className="featured--seasons">{item.number_of_seasons} {item.number_of_seasons !== 1 ? "temporadas" : "temporada"}</div>
+                        <div className="featured--description">{item.overview}</div>
+                        <div className="featured--buttons">
+                            <a className="featured--watchbutton" href={`/watch/${item.id}`}>Assistir</a>
+                            <a className="featured--mylistbutton" href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                        </div>
+                        <div className="featured--genres"><strong>Genero:</strong> {genres.join(', ')}</div>
                     </div>
                 </div>
             </div>
